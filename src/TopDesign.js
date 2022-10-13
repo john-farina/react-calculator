a26ad7684d68c14452c2a0fe701351034eedb9cf
onefootfarina <clichecontent@gmail.com>
2022-10-13 15:17:39 -0400

    screen texture and pi and squared buttons nice

---
+++ src/TopDesign.js
@@ -0,0 +1,22 @@
+export default function TopDesign() {
+  return (
+    <>
+      <div className="topHolder">
+        <div className="middleOpening"></div>
+        <div className="textContainer">
+          <p>pocket calculator</p>
+        </div>
+      </div>
+
+      <div className="top">
+        <div className="topLogos">
+          <p className="KOtext">
+            J . F <span className="exPoint">!</span>
+          </p>
+        </div>
+        <div className="topLogos"></div>
+        <div className="topLogos"></div>
+      </div>
+    </>
+  );
+}
