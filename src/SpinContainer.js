85b6d09a044731b7cda90aa0f7d0c4b2ad097508
onefootfarina <clichecontent@gmail.com>
2022-10-12 22:49:29 -0400

    wow animation spin lol why

--- src/SpinContainer.js
+++ src/SpinContainer.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 export default function SpinContainer({ spin }) {
   console.log(spin);
+  let [animClass, setAnimClass] = useState("");
+  let [animation, setAnimation] = useState(false);
+
+  function addSpinAndRemove() {
+    if (!animation) {
+      setAnimation(true);
+      setAnimClass("spinAnimate");
+      setTimeout(() => {
+        setAnimClass("");
+        setAnimation(false);
+      }, 900);
+    }
+  }
+
   return (
     <div className="bigSpinContainer">
       <div className="backing">
@@ -7,8 +23,13 @@ export default function SpinContainer({ spin }) {
         <div className="longRows"></div>
         <div className="longRows"></div>
       </div>
-      <div className="buttonContainer">
-        <div className="button">
+      <div
+        className="buttonContainer"
+        onClick={() => {
+          addSpinAndRemove();
+        }}
+      >
+        <div className={`button ${animClass}`}>
           <div
             className="buttonIndent"
             style={{ transform: `rotate(${spin}deg)` }}
